0c13bdb10e273a1b7a08d1a26d238ac385e6906e
Praveen Kumar Goswami <44088638+praveengoswami143@users.noreply.github.com>
2021-05-16 16:39:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const toggleMenu = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+toggleMenu.onclick = function(){
+    toggleMenu.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
